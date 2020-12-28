fab6feae3a81137b693bafb923bcc719dca1758d
FrozenRa <ngocnam3518@gmail.com>
2020-12-28 16:23:21 +0700

    final update

--- cmt.py
+++ cmt.py
@@ -28,14 +28,16 @@ sleep(10)
 # 3. Mở URL link
 browser.get("https://www.facebook.com/mathtasyvn/photos/a.4705547702853485/4713897892018466/")
 sleep(random.randint(5,10))
-txtComment = browser.find_element_by_xpath("/html/body/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]")
-# complementary
+txtComment = browser.find_element_by_xpath("/html/body/div[1]/div/div[1]/div[1]/div[3]/div/div/div[1]/div[1]/div/div[2]/div/div/div/div[1]/div[5]/div/div[2]/div/div/div/div/form/div/div/div[2]/div")
 
-txtComment.send_keys("Linh Linh Nguyễn #Mathtasy #Chusovathegioi")
-txtComment.send_keys(Keys.ENTER)
-sleep(600)
-txtComment.send_keys("Linh Linh Nguyễn #Mathtasy #Chusovathegioi")
-txtComment.send_keys(Keys.ENTER)
-sleep(8)
+t = [300, 578, 674, 968, 1283, 1253, 1783, 1581, 783, 1023, 60, 198, 234, 119]
+while True:
+    temp = random.choice(t)
+    print("Time to next comment:" + str(temp) +"s")
+    txtComment.send_keys("Linh Linh Nguyễn #Mathtasy #Chusovathegioi")
+    txtComment.send_keys(Keys.ENTER)
+    sleep(temp)
+    
+
 # 6. Đóng browser
 browser.close()
