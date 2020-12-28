bf2798b885eb3052339cc644500c5efe74c5509e
FrozenRa <ngocnam3518@gmail.com>
2020-12-28 15:28:01 +0700

    login before comment

--- cmt.py
+++ cmt.py
@@ -1,11 +1,41 @@
-import time
-
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from time import sleep
+from selenium.webdriver.common.keys import Keys
+import random
 
-service = Service('./chromedriver.exe')
-service.start()
-driver = webdriver.Remote(service.service_url)
-driver.get('https://www.facebook.com/mathtasyvn/photos/a.4705547702853485/4713897892018466/')
-time.sleep(5) # Let the user actually see something!
-driver.quit()
+# 1. Khai báo browser
+browser = webdriver.Chrome(executable_path="./chromedriver.exe")
+
+# 2. Đăng nhập
+browser.get("https://www.facebook.com/")
+sleep(random.randint(5,10))
+with open("account.txt") as f:
+    acc = f.readlines()
+    email = acc[0].split(":")[1].replace("\n", "")
+    password = acc[1].split(":")[1].replace("\n", "")
+
+# 2a. Điền thông tin vào ô user và pass
+txtUser = browser.find_element_by_id("email")
+txtUser.send_keys(email) # <---  Điền username thật của các bạn vào đây
+
+txtPass = browser.find_element_by_id("pass")
+txtPass.send_keys(password)
+
+# 2b. Submit form
+txtPass.send_keys(Keys.ENTER)
+sleep(10)
+
+# 3. Mở URL link
+browser.get("https://www.facebook.com/mathtasyvn/photos/a.4705547702853485/4713897892018466/")
+sleep(random.randint(5,10))
+txtComment = browser.find_element_by_xpath("/html/body/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]")
+# complementary
+
+txtComment.send_keys("Linh Linh Nguyễn #Mathtasy #Chusovathegioi")
+txtComment.send_keys(Keys.ENTER)
+sleep(600)
+txtComment.send_keys("Linh Linh Nguyễn #Mathtasy #Chusovathegioi")
+txtComment.send_keys(Keys.ENTER)
+sleep(8)
+# 6. Đóng browser
+browser.close()
