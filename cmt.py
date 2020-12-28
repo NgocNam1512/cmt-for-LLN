69a82c8b25f2090360f57f900588a3fb6b72e954
FrozenRa <ngocnam3518@gmail.com>
2020-12-28 14:55:44 +0700

    update relative driver link

--- cmt.py
+++ cmt.py
@@ -3,7 +3,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
-service = Service('C:/Users/namnn12/Documents/Project/cmt-for-LLN/chromedriver.exe')
+service = Service('./chromedriver.exe')
 service.start()
 driver = webdriver.Remote(service.service_url)
 driver.get('https://www.facebook.com/mathtasyvn/photos/a.4705547702853485/4713897892018466/')
